8b08c7b8dcc089de6ba62ec8a5fa187c8fb9f631
Brandon Korous <me@brandonkorous.com>
2025-04-23 21:46:38 -0700

    feat: enhance toMongoFilter to handle unsupported operators more robustly

--- src/mongo/utils.ts
+++ src/mongo/utils.ts
@@ -10,6 +10,17 @@ export function toMongoFilter(filter: Record<string, any>) {
     return tracer.startSpan('utils.toMongoFilter', async () => {
         logger.debug?.('Building MongoDB filter', { filter })
         // TODO: Add support for more advanced filters ($gt, $in, etc.)
+        for (const key in filter) {
+            if (typeof filter[key] === 'object' && filter[key] !== null) {
+            for (const operator in filter[key]) {
+                if (['$gt', '$gte', '$lt', '$lte', '$in', '$nin', '$ne'].includes(operator)) {
+                filter[key] = { ...filter[key] }
+                } else {
+                throw new Error(`Unsupported operator: ${operator}`)
+                }
+            }
+            }
+        }
         return filter
     })
 }
